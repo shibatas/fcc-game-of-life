7621d8320144537e1c4154dc71e9d103e8d90807
shibatas <shohei51@gmail.com>
2017-11-01 22:01:29 +0100

    table initial rendering by api get data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 import $ from 'jquery';
 import './App.css';
 
+var DATA = []
 //url for last 30 days leader
 var url1 = 'https://fcctop100.herokuapp.com/api/fccusers/top/recent';
 //url for all time leaders
 var url2 = 'https://fcctop100.herokuapp.com/api/fccusers/top/alltime';
-class App extends Component {
+
+class Top extends Component {
   render() {
     return (
       <div className="header">
@@ -16,60 +17,85 @@ class App extends Component {
     );
   }
 }; //learn how to add children to App Component
-class titleRow extends Component {
+class TitleRow extends Component {
   render() {
     //learn to loop the th tag elements
     return (
-      <tr>
-        <th>Rank</th>
-        <th>User</th>
-        <th>Last 30 Days</th>
-        <th>All Time</th>
-      </tr>
+      <thead>
+        <tr>
+          <th>Rank</th>
+          <th>User</th>
+          <th>Last 30 Days</th>
+          <th>All Time</th>
+        </tr>
+      </thead>
     );
   }
 };
-class table extends Component {
+class DataRow extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+        data: DATA
+      }
+  }
+  componentDidMount() {
+    $.getJSON(url1).then(result => {
+      this.setState({
+        data: result
+      });
+    });
+  }
+
   render() {
+    const rows = [];
+    this.state.data.forEach(function(item, index) {
+      rows.push(
+        <tr key={item.username}>
+          <td>{index + 1}</td>
+          <td>{item.username}</td>
+          <td>{item.recent}</td>
+          <td>{item.alltime}</td>
+        </tr>
+      );
+    });
+
     return (
-      <table>
-        <titleRow />
-      </table>
+      <tbody>
+        {rows}
+      </tbody>
     );
   }
 };
-class footer extends Component {
+class Footer extends Component {
   render() {
     return (
       <footer>
         <hr/>
-          <p>This is a <a href="https://www.freecodecamp.org/challenges/build-a-markdown-previewer" target="_blank">freeCodeCamp project</a>.
-          See my other work on <a class="fa fa-github fa-2x" aria-hidden="true" href="https://github.com/shibatas/" target="_blank"></a> and
-          <a class="fa fa-codepen fa-2x" aria-hidden="true" href="https://codepen.io/Shohei51/" target="_blank"></a></p>
+          <p>This is a <a href="https://www.freecodecamp.org/challenges/build-a-camper-leaderboard" target="_blank" rel="noopener noreferrer">freeCodeCamp project</a>.
+          See my other work on <a className="fa fa-github fa-2x" aria-hidden="true" href="https://github.com/shibatas/" target="_blank" rel="noopener noreferrer"></a> and
+          <a className="fa fa-codepen fa-2x" aria-hidden="true" href="https://codepen.io/Shohei51/" target="_blank" rel="noopener noreferrer"></a></p>
           <p>Shohei Shibata &#9426; copyright 2017</p>
       </footer>
     );
   }
 };
-
-$(document).ready(function() {
-  console.log('test');
-  $.getJSON(url1, function(data) {
-    console.log(data[0]);
-  });
-});
-/*
 class App extends Component {
   render() {
     return (
       <div>
-        <top />
-        <table />
+        <Top />
+        <table>
+          <TitleRow />
+          <DataRow />
+        </table>
+        <Footer />
       </div>
+
     );
   }
 };
-*/
+
 /* Use ReactDOM for CodePen
 ReactDOM.render(
     React.createElement('div', {},
