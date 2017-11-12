8f8b92213b492d51bd3eaeb0bc67eb9011acbfd1
shibatas <shohei51@gmail.com>
2017-11-12 17:53:55 +0100

    tuning frame rate...

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 //number of cells, [x, y]
 const size = [50, 30];
 //default speed, milliseconds frame rate
-const rate = 100;
+const rate = 250;
 
 //density of live cells when generating random board
 const density = 0.3;
