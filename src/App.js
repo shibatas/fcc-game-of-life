2ba485d66cf724f7f5890768b294ca9ea961e2d5
shibatas <shohei51@gmail.com>
2017-11-12 11:08:57 +0100

    basic board live/dead updates frame-by-frame

--- src/App.js
+++ src/App.js
@@ -75,7 +75,12 @@ class Board extends Component {
     }
     return (
       <div>
-        {box}
+        <div className="board">
+          {box}
+        </div>
+        <div>
+          <button id="nextStep" onClick={this.step}>Next Step</button>
+        </div>
       </div>
     );
   }
@@ -90,6 +95,53 @@ class Board extends Component {
       newState[id] = 'dead';
     }
     this.setState(newState);
+  }
+  step = () => {
+    //handles update according to game rules
+    //Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
+    //Any live cell with two or three live neighbours lives on to the next generation.
+    //Any live cell with more than three live neighbours dies, as if by overpopulation.
+    //Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+    let currentBoard = this.state;
+    let newBoard = {};
+    for (const prop in currentBoard) {
+      newBoard[prop] =  this.nextState(prop);
+    }
+    this.setState(newBoard);
+  }
+  nextState = (id) => {
+    let oldState = this.state[id];
+    let current = id.split('x');
+    let x = parseInt(current[0]);
+    let y = parseInt(current[1]);
+    //Get coordinates of all neighboring cells
+    let neighbors = [];
+    for (let i=-1; i<2; i++) {
+      for (let j=-1; j<2; j++) {
+        neighbors.push([(x+i)+'x'+(y+j)]);
+      }
+    }
+    neighbors.splice(4,1);
+    //get sum of surrounding live cells
+    let sum = neighbors.reduce((sum, val) => {
+      if (this.state[val] === 'live') {
+        return sum+1;
+      } else { return sum }
+    }, 0);
+    //return the appropriate new state
+    let newState = '';
+    if (oldState === 'dead') {
+      if (sum === 3) {
+        newState = 'live';
+      } else {
+        newState = 'dead';
+      }
+    } else if (sum < 2 || sum > 3) {
+      newState = 'dead';
+    } else {
+      newState = 'live';
+    }
+    return newState;
   }
 }
 
